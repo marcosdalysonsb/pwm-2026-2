c221d70b6a6a2a28b1e62add70b575613451f76c
Marcos Dalyson <marcos.00000854567@unicap.br>
2026-09-22 11:57:17 +0000

    Alterações

--- react-03/api/index.js
+++ react-03/api/index.js
@@ -6,8 +6,8 @@ const instance = axios.create({
   baseURL: "https://parseapi.back4app.com",
   timeout: 5000,
   headers: {
-    "X-Parse-Application-Id": "t4wZSEfltZ4QtFBZX5LY9hyyx4I45uAbjsrwzCAi",
-    "X-Parse-REST-API-Key": "ZXx7xAlvFOyZith0HDnSpWdlHxnnATKsdgakYg4Q",
+    "X-Parse-Application-Id": "trdTq2pgqqrFJtmeKX6vt5PkR0ChrHJfmXwXc98Y",
+    "X-Parse-REST-API-Key": "zsYznxmw4mzDCoLi97l5iIR5FYF9yALFOzPHjvF2",
   },
 });
 
